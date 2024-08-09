f10fa4d0ba7c230c523472fdaf9ec22fd11a7b46
MOHD ARSLAN <64858711+QUANTUM00007@users.noreply.github.com>
2024-08-09 22:18:58 +0530

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -152,7 +152,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'admin@myshop.com'
-EMAIL_HOST_PASSWORD = 'Arslan123'
+EMAIL_HOST_PASSWORD = 'xxxxxx'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
